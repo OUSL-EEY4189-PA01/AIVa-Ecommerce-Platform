09c533b1696c7d2615fb4a2cfe19dfdce3c12724
MZ NAVEED <216429747+MZ-NAVEED@users.noreply.github.com>
2025-10-01 12:28:38 +0530

    Initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 class="text-5xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 drop-shadow-lg text-center mt-10">
+  AIVa
+</h1>
+
+    </>
+  )
+}
+
+export default App
