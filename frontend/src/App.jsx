a029d07e7a817e1021fd96fb3f310c058e4d6802
MZ NAVEED <216429747+MZ-NAVEED@users.noreply.github.com>
2025-10-03 13:32:04 +0530

    Added sample navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,28 @@ function App() {
 
   return (
     <>
-      <h1 class="text-5xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 drop-shadow-lg text-center mt-10">
-  AIVa
-</h1>
+      <nav className="bg-white shadow-md">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center">
+          <div className="text-purple-600 font-extrabold text-2xl cursor-pointer">
+            AIVa
+          </div>
 
+          <div class="hidden md:flex space-x-12 text-gray-700 font-medium ml-20">
+            <a href="#" className="hover:text-purple-600 transition">Home</a>
+            <a href="#" className="hover:text-purple-600 transition">Products</a>
+            <a href="#" className="hover:text-purple-600 transition">Chat</a>
+          </div>
+
+          <div className="flex space-x-4 ml-auto">
+            <button className="px-4 py-2 border border-purple-600 text-purple-600 rounded-lg hover:bg-purple-50 transition">
+              Login
+            </button>
+            <button className="px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition">
+              Register
+            </button>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
