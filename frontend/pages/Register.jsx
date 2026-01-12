428de9faf15bf38f4b65ce195b19ecaf01e83cdc
ASUS <dinithin2ooo@gmail.com>
2026-01-12 13:42:33 +0530

    update register form

--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -4,15 +4,37 @@ import axios from "axios";
 
 const Register = () => {
   const [name, setName] = useState('');
+  const [birthdate, setBirthdate] = useState('');
   const [email, setEmail] = useState('');
+  const [address, setAddress] = useState({
+    houseNo: "", 
+    street: "",
+    city: "",
+    district: "",
+    postalcode: "",
+    country: ""
+  });
+
+  const handleAddressChange= (e) => {
+    const { name, value } = e.target;
+    setAddress({ ...address, [name]: value });
+  };
+
+  const [contactno, setContactno] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmpassword] = useState('');
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (password !== confirmpassword) {
+      alert("Passwords do not match!");
+      return;
+    }
     setError('');
     setSuccess('');
     setLoading(true);
@@ -43,12 +65,23 @@ const Register = () => {
           <input
             type="text"
             name="name"
-            placeholder="Name"
+            placeholder="Full Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
             className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
             required
           />
+
+          <input
+            type="date"
+            name="birthdate"
+            placeholder="Birth Date"
+            value={birthdate}
+            onChange={ (e) => setBirthdate(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+            required
+          />
+
           <input
             type="email"
             name="email"
@@ -58,12 +91,91 @@ const Register = () => {
             className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
             required
           />
+
+          <input
+            type="text" 
+            name="houseNo"
+            placeholder="House No"
+            value={address.houseNo}
+            onChange={handleAddressChange}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+          />
+
+          <input
+            type="text"
+            name="street"
+            placeholder="Street/Lane"
+            value={address.street}
+            onChange={handleAddressChange}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+          />
+
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={address.city}
+            onChange={handleAddressChange}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+          />
+
+          <input
+            type="text"
+            name="district"
+            placeholder="District"
+            value={address.district}
+            onChange={handleAddressChange}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+          />
+
+          <input
+            type="number"
+            name="postalcode"
+            placeholder="Postal Code"
+            value={address.postalcode}
+            onChange={handleAddressChange}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+          />
+
+          <input
+            type="text"
+            name="street"
+            placeholder="Street/Lane"
+            value={address.street}
+            onChange={handleAddressChange}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+          />
+
+          <select class="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black">
+            <option>Sri Lanka</option>
+          </select>
+
+          <input
+            type="number"
+            name="contactno"
+            placeholder="Contact Number"
+            value={contactno}
+            onChange={(e) => setContactno(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+            required         
+          />
+
           <input
             type="password"
             name="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="w-full mb-6 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
+            required
+          />
+
+          <input
+            type="password"
+            name="confirmpassword"
+            placeholder="Confirm Password"
+            value={confirmpassword}
+            onChange={(e) => setConfirmpassword(e.target.value)}
             className="w-full mb-6 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-600 bg-white text-black"
             required
           />
